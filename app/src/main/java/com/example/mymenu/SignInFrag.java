e93c87d5d3782c67b0898e77632ab2b6b4b06244
Shaked Naim <shakednaim2006@gmail.com>
2023-11-26 15:29:28 +0200

    chang every thing to frag and created singIn

---
+++ app/src/main/java/com/example/mymenu/SignInFrag.java
@@ -0,0 +1,44 @@
+package com.example.mymenu;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+    public class SignInFrag extends Fragment {
+
+    private DatabaseHelper myDb;
+    private EditText name, surename;
+    private Button btnCheck;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_up, container, false);
+
+        myDb = new DatabaseHelper(requireContext());
+
+        name = view.findViewById(R.id.etName);
+        surename = view.findViewById(R.id.etSure);
+
+        btnCheck = view.findViewById(R.id.btnCheck);
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(myDb.dataExists(name.getText().toString(), surename.getText().toString()))
+                    Toast.makeText(getContext(), "Data Exists", Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(getContext(), "Data Not Exists", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return view;
+    }
+}
